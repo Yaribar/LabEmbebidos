7a618652c757862a27268396879f1c05f584bb8b
Yaribar <40440445+Yaribar@users.noreply.github.com>
2022-10-19 11:41:13 -0500

    Scale Function to C

--- Laboratorio3/ImagePNG.c
+++ Laboratorio3/ImagePNG.c
@@ -20,14 +20,19 @@ uint16_t height)
 	}
 }
 
-void resize( color[] img,int width_img,int height_img,int x, int y,int scale){
+void DrawImage(
+et024006_color_t const * pixmap,
+uint16_t width_img,
+uint16-t height_img,
+uint16_t x,
+uint16_t y,
+uint8_t scale){
   a=0;
   for(int i = 0; i< height_img*scale;i+=1*scale){
     for(int j = 0; j< width_img*scale;j++){
       for(int k = 0; k<scale;k++){
-        stroke(img[a/scale]);
-        point(x+j,y+i+k);
-        //println(a/scale);
+        //stroke(img[a/scale]);
+	et024006_DrawPixel( x+j, y+i, *pixmap+=a/scale );
       } 
       if(a<width_img*height_img*(scale)-1){
         a++;
