9e38686f5c13f374da3aa8da81885969d21a18ca
Yaribar <yarib.soccer@gmail.com>
2022-11-06 10:46:24 -0600

    Char array functions added

--- Laboratorio4/EjemploUSART.c
+++ Laboratorio4/EjemploUSART.c
@@ -87,6 +87,8 @@ int main(void)
 	usart_write_line(EXAMPLE_USART,player_one.name);
 	
 	while(1);
+
+    player_one.modality=atoi(usart_read_line(EXAMPLE_USART)); //Atoi converts char array to int number
 	
 }
 
@@ -106,3 +108,51 @@ void usart_read_line(char *lineRead, size_t len){
 }
 
 
+
+char* convertIntegerToChar(int N)
+{
+ 
+    // Count digits in number N
+    int m = N;
+    int digit = 0;
+    while (m) {
+ 
+        // Increment number of digits
+        digit++;
+        m /= 10;
+    }
+ 
+    // Declare char array for result
+    char* arr;
+    // Declare duplicate char array
+    char arr1[digit];
+    // Memory allocation of array
+    arr = (char*)malloc(digit);
+ 
+    // Separating integer into digits and
+    // accommodate it to character array
+    int index = 0;
+    while (N) {
+ 
+        // Separate last digit from
+        // the number and add ASCII
+        // value of character '0' is 48
+        arr1[++index] = N % 10 + '0';
+ 
+        // Truncate the last
+        // digit from the number
+        N /= 10;
+    }
+ 
+    // Reverse the array for result
+    int i;
+    for (i = 0; i < index; i++) {
+        arr[i] = arr1[index - i];
+    }
+ 
+    // Char array truncate by null
+    arr[i] = '\0';
+ 
+    // Return char array
+    return (char*)arr;
+}
